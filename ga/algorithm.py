1339c6a99453be60868d4a07e76593e8908640ea
Serhii Vernydub <svernidub@gmail.com>
2017-06-25 20:52:16 +0300

    Add mutation.

--- ga/algorithm.py
+++ ga/algorithm.py
@@ -2,9 +2,11 @@ from initialize_population import InitializePopulation
 from calculate_function_value import CalculateFunctionValue
 from reproduction_selection import ReproductionSelection
 from crossingover import Crossingover
+from mutation import Mutation
 
 class Algorithm:
-    def __init__(self, fitness_function, x_min, x_max, population_size, mutation_probability):
+    def __init__(self, fitness_function, x_min, x_max, population_size,
+                mutation_probability):
         self.fitness_function = fitness_function
         self.x_min = x_min
         self.x_max = x_max
@@ -16,16 +18,19 @@ class Algorithm:
         self.__initialize_population()
         self.__calculate_function_value()
         self.__reproduction_selection()
-        self.__crossing_over()
+        self.__crossingover()
+        self.__mutation()
 
 
     def __initialize_population(self):
-        initializator = InitializePopulation(self.population_size, self.x_min, self.x_max)
+        initializator = InitializePopulation(self.population_size,
+                                             self.x_min, self.x_max)
         self.__population = initializator.perform()
 
 
     def __calculate_function_value(self):
-        calculator = CalculateFunctionValue(self.__population, self.fitness_function)
+        calculator = CalculateFunctionValue(self.__population,
+                                            self.fitness_function)
         calculator.perform()
 
 
@@ -34,13 +39,18 @@ class Algorithm:
         self.__parents = reproductor.perform()
 
 
-    def __crossing_over(self):
+    def __crossingover(self):
         crosser = Crossingover(self.__parents)
         self.__childs = crosser.perform()
+
+
+    def __mutation(self):
+        mutator = Mutation(self.__childs, self.mutation_probability)
+        mutator.perform()
 
 
 import math
 func = lambda x: math.sin(x) * 3 + 3 * x + 3
 
-a = Algorithm(func, 0, 10, 100, 0.0002)
+a = Algorithm(func, 0, 10, 10000, 0.02)
 a.perform()
--- ga/mutation.py
+++ ga/mutation.py
@@ -1,19 +1,40 @@
 #-*-coding: utf-8 -*-
 
+import math
+import random
+
+from individual import Individual
+
 class Mutation:
     """
     Производит мутацию у потомков
     """
 
-    def __init__(childs, probability):
+    def __init__(self, childs, probability):
         """
         Получает массив особей-потомков и вероятность мутации
         """
-        None
+        self.childs = childs
+        self.probability = probability
 
 
     def perform(self):
         """
         Выполняет мутацию с заданной вероятностью
         """
-        None
+        for i, ind in enumerate(self.childs):
+            self.__mutate(i, ind)
+
+
+    def __mutate(self, i, ind):
+        if not self.__will_mutate(): return
+
+        genes = list(ind.genes())
+        mutation_gen = int(math.floor(random.random() * len(genes)))
+        genes[mutation_gen] = str(1 -int(genes[mutation_gen]))
+        ind = Individual()
+        ind.load_genes(''.join(genes))
+        self.childs[i] = ind
+
+    def __will_mutate(self):
+        return random.random() <= self.probability
